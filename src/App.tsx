33aee2313dc5e00b35e9aea32d3040f3305b9a0b
Mostafa <candlelearner.m.dev@gmail.com>
2025-02-21 17:48:36 +0100

    Spliting the files into multiple files

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import About from "./Components/About";
+import Hero from "./Components/Hero";
+import NavBar from "./Components/navBar";
+import Services from "./Components/Services";
+import Tours from "./Components/Tours";
+
 export default function App() {
-  return <div></div>
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+    </>
+  );
 }
